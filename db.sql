4567469e176ed92ad8d110b63f22bf73d6003f01
Asyary Raihan <asyaryraihan@gmail.com>
2026-06-10 01:24:05 +0700

    Add phone number and DOB column

--- db.sql
+++ db.sql
@@ -38,6 +38,8 @@ CREATE TABLE users (
     email VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
     full_name VARCHAR(255) NOT NULL,
+    phone_number VARCHAR(20),
+    date_of_birth DATE,
     role VARCHAR(50) DEFAULT 'member' CHECK (role IN ('member', 'pengurus')),
     is_verified BOOLEAN DEFAULT FALSE,
     penalty_amount NUMERIC(10, 2) DEFAULT 0, -- Stacks 5k for missed tap-outs
