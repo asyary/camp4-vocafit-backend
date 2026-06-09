446034012a93b9eb4879bd60dcd7b72f48fb7a48
Asyary Raihan <asyaryraihan@gmail.com>
2026-06-10 04:37:39 +0700

    Add profile thumbnails

--- src/modules/pengurus/pengurus.service.js
+++ src/modules/pengurus/pengurus.service.js
@@ -1,6 +1,7 @@
 const repository = require('./pengurus.repository');
 const cloudinary = require('../../config/cloudinary');
 const bcrypt = require('bcrypt');
+const { withProfileImageThumb } = require('../../utils/image.util');
 
 const uploadToCloudinary = (fileBuffer, folder) => {
     return new Promise((resolve, reject) => {
@@ -30,14 +31,14 @@ const getUsersList = async (page, limit) => {
         page,
         limit,
         total_pages: totalPages,
-        data: users
+        data: users.map(user => withProfileImageThumb(user))
     };
 };
 
 const getUserById = async (id) => {
     const user = await repository.getUserById(id);
     if (!user) throw new Error('User not found');
-    return user;
+    return withProfileImageThumb(user);
 };
 
 const addUser = async (data, fileBuffer) => {
@@ -46,12 +47,13 @@ const addUser = async (data, fileBuffer) => {
     const passwordHash = await bcrypt.hash(data.password, 12);
     const imageUrl = await uploadToCloudinary(fileBuffer, 'users');
 
-    return await repository.createUser({
+    const newUser = await repository.createUser({
         ...data,
         passwordHash,
         profileImageUrl: imageUrl,
         penaltyAmount: data.penaltyAmount || 0
     });
+    return withProfileImageThumb(newUser);
 };
 
 const editUser = async (id, data, fileBuffer) => {
@@ -71,7 +73,7 @@ const editUser = async (id, data, fileBuffer) => {
         profileImageUrl
     });
     if (!updatedUser) throw new Error('User not found');
-    return updatedUser;
+    return withProfileImageThumb(updatedUser);
 };
 
 const removeUser = async (id) => {
